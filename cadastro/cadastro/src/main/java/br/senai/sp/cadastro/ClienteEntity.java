c384128df012e360fd1d6c2e757dbdf4c5bd48ad
stephany <stephanysousah123@gmail.com>
2025-06-01 12:24:48 -0300

    Criando Classe Entity e Cliente

--- cadastro/cadastro/src/main/java/br/senai/sp/cadastro/ClienteEntity.java
+++ cadastro/cadastro/src/main/java/br/senai/sp/cadastro/ClienteEntity.java
@@ -1,6 +1,19 @@
 package br.senai.sp.cadastro;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clientes")
 public class ClienteEntity {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
 }
