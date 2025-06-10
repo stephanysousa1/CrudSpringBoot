82faaa9d47c819fba2d5d210536d2e2920a0acd7
stephany <stephanysousah123@gmail.com>
2025-06-10 12:23:18 -0300

    feat: adicionando novos atributos

--- cadastro/cadastro/src/main/java/br/senai/sp/cadastro/ClienteEntity.java
+++ cadastro/cadastro/src/main/java/br/senai/sp/cadastro/ClienteEntity.java
@@ -37,12 +37,5 @@ public class ClienteEntity {
     @Column(nullable = false)
     private String cep;
 
-    // método para calcular a idade baseado na data de nascimento
-
-    public int getIdade() {
-        if (this.dataNascimento == null) return 0;
-        return Period.between(this.dataNascimento, LocalDate.now()).getYears();
-    }
-
 }
 
